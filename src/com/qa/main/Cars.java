e818ac68fe22784be863cb25389978c1527af001
Kajan Kugananthajothy <kajankugananthajothy@gmail.com>
2022-05-13 11:51:19 +0100

    Vehicle class initial setup

--- src/com/qa/main/Cars.java
+++ src/com/qa/main/Cars.java
@@ -1,5 +1,20 @@
 package com.qa.main;
 
-public class Cars {
+public class Cars extends Vehicle {
+	//Car specifc attribute
+	private float topSpeed;
 
+	//Constructor
+	public Cars(String engineType, int range, boolean cargoStorage, float topSpeed) {
+		super(engineType, range, cargoStorage);
+		this.topSpeed = topSpeed;
+	}
+
+	@Override
+	public String fixVehicle() {
+		// Need to do the fix vehicle code
+		return null;
+	}
+	
+	
 }
--- src/com/qa/main/Vehicle.java
+++ src/com/qa/main/Vehicle.java
@@ -1,9 +1,45 @@
 package com.qa.main;
 
 public abstract class Vehicle {
+	//Attributes
 	private String engineType;
 	private int range;
 	private boolean cargoStorage;
 	
-	public abstract fixVehicle();
+	//Constructor
+	public Vehicle(String engineType, int range, boolean cargoStorage) {
+		super();
+		this.engineType = engineType;
+		this.range = range;
+		this.cargoStorage = cargoStorage;
+	}
+	
+	//Abstract method
+	public abstract String fixVehicle();
+	
+	//Getters and Setters
+	public String getEngineType() {
+		return engineType;
+	}
+
+	public void setEngineType(String engineType) {
+		this.engineType = engineType;
+	}
+
+	public int getRange() {
+		return range;
+	}
+
+	public void setRange(int range) {
+		this.range = range;
+	}
+
+	public boolean isCargoStorage() {
+		return cargoStorage;
+	}
+
+	public void setCargoStorage(boolean cargoStorage) {
+		this.cargoStorage = cargoStorage;
+	}
+	
 }
