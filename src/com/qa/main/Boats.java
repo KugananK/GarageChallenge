1b335b45199036a8594ab509f7c7291c777ceca0
Kajan Kugananthajothy <kajankugananthajothy@gmail.com>
2022-05-13 13:03:40 +0100

    Attempt at fixvehicle code

--- src/com/qa/main/Boats.java
+++ src/com/qa/main/Boats.java
@@ -2,19 +2,23 @@ package com.qa.main;
 
 public class Boats extends Vehicle {
 	
+	//Boat specific attribute
 	private float price;
 
+	//Boat constructor
 	public Boats(String engineType, int range, boolean cargoStorage, float price) {
 		super(engineType, range, cargoStorage);
 		this.price = price;
 	}
 
+	//Abstract method
 	@Override
 	public String fixVehicle() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	//Boat specific getter and setter
 	public float getPrice() {
 		return price;
 	}
--- src/com/qa/main/Lorries.java
+++ src/com/qa/main/Lorries.java
@@ -2,19 +2,23 @@ package com.qa.main;
 
 public class Lorries extends Vehicle {
 	
+	//Lorry specific attributes
 	private int weight;
 
+	//Lorry constructor
 	public Lorries(String engineType, int range, boolean cargoStorage, int weight) {
 		super(engineType, range, cargoStorage);
 		this.weight = weight;
 	}
 
+	//Abstract method
 	@Override
 	public String fixVehicle() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	//Lorry specific getter and setter
 	public int getWeight() {
 		return weight;
 	}
